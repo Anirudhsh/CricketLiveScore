0ef0226fd11ce72fcee8f4fc356270810bf79bb6
Anirudhsh <37963152+Anirudhsh@users.noreply.github.com>
2019-06-15 22:18:34 +0530

    web scaping file to extract the score
    Extracts the score from a particular site and prints it on the screen

---
+++ scric.py
@@ -0,0 +1,19 @@
+#Python program to scrape website 
+#and save quotes from website 
+import requests 
+from bs4 import BeautifulSoup
+
+URL = "https://www.cricbuzz.com/cricket-match/live-scores"
+r = requests.get(URL) 
+
+soup = BeautifulSoup(r.content, 'html5lib') 
+
+quotes=[] # a list to store quotes 
+
+table = soup.find('div', attrs = {'id':'page-wrapper'}).find('div', attrs = {'class':'cb-bg-white cb-col-100 cb-col'}).find('div', attrs = {'class':'cb-col-67 cb-col cb-left cb-schdl'}).find('div', attrs = {'class':'cb-col cb-col-100 cb-lv-main'}) 
+currtable = soup.find('div', attrs = {'class':'cb-lv-scrs-col text-black'})
+targtable = soup.find('div', attrs = {'class':'cb-lv-scrs-col cb-text-live'})
+score=currtable.text+"\n"+targtable.text
+print(score)
+    
+#   .find('div', attrs = {'class':'cb-col-100 cb-col cb-schdl'}).find('div', attrs = {'class':'cb-col'})
